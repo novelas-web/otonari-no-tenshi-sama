e721fd0be22096eea04a1c06ca7aa28d6b64b362
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2023-12-05 15:52:47 -0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,11 +1,44 @@
-function emer(event) {
+document.getElementById('enlace').addEventListener('click', function (event) {
+      event.preventDefault(); // Evitar que el enlace redireccione
+    
+    var rutaArchivo = this.getAttribute('href');
+    cargarContenido(rutaArchivo);
+  });
 
-      var seccion = document.getElementById('mi-seccion');
-      seccion.innerHTML = '<div class="vent"> <iframe src="#" name="vent"></iframe> <a href="#" onclick="cerr(event)">X</a></div>';
+  function cargarContenido(rutaArchivo) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', rutaArchivo, true);
 
-}
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var contenidoTexto = xhr.responseText;
+        var contenidoHTML = formatearContenido(contenidoTexto);
 
-function cerr(event) {
-      var seccion = document.getElementById('mi-seccion');
-      seccion.innerHTML = '';
+        crearDiv(contenidoHTML);
+      }
+    };
+
+    xhr.send();
+  }
+
+  function formatearContenido(contenidoTexto) {
+    var lineas = contenidoTexto.split('\n');
+    var contenidoHTML = '';
+
+    for (var i = 0; i < lineas.length; i++) {
+      contenidoHTML += '<p>' + lineas[i] + '</p>';
     }
+
+    return contenidoHTML;
+  }
+
+  function crearDiv(contenidoHTML) {
+    var seccion = document.getElementById('seccion');
+    var nuevoDiv = document.createElement('div');
+    nuevoDiv.className = 'contenido';
+    nuevoDiv.innerHTML = contenidoHTML;
+    
+    seccion.appendChild(nuevoDiv);
+    
+    nuevoDiv.style.display = 'block'; // Mostrar el contenido
+  }
