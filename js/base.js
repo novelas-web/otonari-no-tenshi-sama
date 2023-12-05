e143eb829a24df26a8b56fd997e32bea3020febd
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2023-12-05 15:59:10 -0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,47 +1,48 @@
 document.getElementById('enlace').addEventListener('click', function (event) {
-      event.preventDefault(); // Evitar que el enlace redireccione
-    
-    var rutaArchivo = this.getAttribute('href');
-    cargarContenido(rutaArchivo);
-  });
+  event.preventDefault(); // Evitar que el enlace redireccione
+  
+  var rutaArchivo = this.getAttribute('href');
+  cargarContenido(rutaArchivo);
+});
 
-  function cargarContenido(rutaArchivo) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', rutaArchivo, true);
+function cargarContenido(rutaArchivo) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', rutaArchivo, true);
 
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        var contenidoTexto = xhr.responseText;
-        var contenidoHTML = formatearContenido(contenidoTexto);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      var contenidoTexto = xhr.responseText;
+      var contenidoHTML = formatearContenido(contenidoTexto);
 
-        crearDiv(contenidoHTML);
-      }
-    };
-
-    xhr.send();
-  }
-
-  function formatearContenido(contenidoTexto) {
-    var lineas = contenidoTexto.split('\n');
-    var contenidoHTML = '';
-
-    for (var i = 0; i < lineas.length; i++) {
-      contenidoHTML += '<p>' + lineas[i] + '</p>';
+      crearDiv(contenidoHTML);
     }
+  };
 
-    return contenidoHTML;
+  xhr.send();
+}
+
+function formatearContenido(contenidoTexto) {
+  var lineas = contenidoTexto.split('\n');
+  var contenidoHTML = '';
+
+  for (var i = 0; i < lineas.length; i++) {
+    contenidoHTML += '<p>' + lineas[i] + '</p>';
   }
 
-  function crearDiv(contenidoHTML) {
-    var seccion = document.getElementById('seccion');
-    var nuevoDiv = document.createElement('div');
-    nuevoDiv.className = 'contenido';
-    nuevoDiv.innerHTML = contenidoHTML;
-    
-    seccion.appendChild(nuevoDiv);
-    
-    nuevoDiv.style.display = 'block'; // Mostrar el contenido
-  }
+  return contenidoHTML;
+}
+
+function crearDiv(contenidoHTML) {
+  var seccion = document.getElementById('seccion');
+  var nuevoDiv = document.createElement('div');
+  nuevoDiv.className = 'contenido';
+  nuevoDiv.innerHTML = contenidoHTML;
+  
+  seccion.appendChild(nuevoDiv);
+  
+  nuevoDiv.style.display = 'block'; // Mostrar el contenido
+}
+
 
 function cerr(event) {
       var seccion = document.getElementById('mi-seccion');
