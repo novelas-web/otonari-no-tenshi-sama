6c09a4ded8f0694d2c54e73109d317d6edb1886f
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2023-12-05 16:23:45 -0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,32 +1,38 @@
 document.getElementById('enlace').addEventListener('click', function(event) {
-    event.preventDefault(); // Evitar que el enlace abra la ruta directamente
+    event.preventDefault(); // Evitar que el enlace redireccione
 
     // Obtener la ruta del archivo desde el atributo href
-    const archivoURL = this.getAttribute('href');
+    var archivoURL = this.getAttribute('href');
 
     // Crear un nuevo div
-    const nuevoDiv = document.createElement('div');
-    nuevoDiv.classList.add('nuevo-div');
+    var nuevoDiv = document.createElement('div');
+    nuevoDiv.className = 'miDiv';
 
-    // Obtener el contenido del archivo de texto
-    fetch(archivoURL)
-        .then(response => response.text())
-        .then(texto => {
-            // Dividir el texto en párrafos
-            const parrafos = texto.split('\n');
-            
-            // Crear elementos de párrafo y añadir al div
-            parrafos.forEach(parrafo => {
-                const p = document.createElement('p');
-                p.textContent = parrafo;
-                nuevoDiv.appendChild(p);
+    // Realizar una solicitud para obtener el contenido del archivo
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', archivoURL, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Obtener el texto del archivo
+            var textoArchivo = xhr.responseText;
+
+            // Dividir el texto en líneas
+            var lineas = textoArchivo.split('\n');
+
+            // Crear párrafos y agregarlos al div
+            lineas.forEach(function(linea) {
+                var parrafo = document.createElement('p');
+                parrafo.textContent = linea;
+                nuevoDiv.appendChild(parrafo);
             });
 
-            // Añadir el nuevo div al contenido
-            document.getElementById('contenido').appendChild(nuevoDiv);
-        })
-        .catch(error => console.error('Error al cargar el archivo:', error));
+            // Agregar el div al section
+            document.getElementById('contenedor').appendChild(nuevoDiv);
+        }
+    };
+    xhr.send();
 });
+
 
 function cerr(event) {
       var seccion = document.getElementById('contenido');
