ff2294ff1011de06f1b8dea9dce1bf00989f7e8b
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2023-12-05 19:49:30 -0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     var enlaceSecundario = document.createElement('a');
                     enlaceSecundario.href = '#'; // Puedes ajustar la URL según tus necesidades
                     enlaceSecundario.classList.add('enlaceSecundario');
-                    enlaceSecundario.textContent = 'X';
+                    enlaceSecundario.textContent = 'Enlace Secundario';
                     enlaceSecundario.onclick = cerrar; // Asociar la función cerrar al evento onclick
 
                     // Agregar el enlace secundario al contenedor
